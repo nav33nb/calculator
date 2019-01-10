3ba4b9c2c7ef7bf2fa22fe8ec7f54cdce2c0af40
nav33nb <naveenbassi33@gmail.com>
2019-01-10 19:01:14 +0530

    Added subtract function Added subtract function

---
+++ calc.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int sub(int num1,int num2){
+  return (num1-num2);
+}
+
+int add(int num1, int num2)
+{
+  return (num1+num2);
+}
+
+int main()
+{
+  fint num1,num2;
+  cout << "Calculator" << endl;
+  cout << "Enter two numbers" <<endl;
+
+  cout << "num1 : ";
+  cin >> num1;
+  cout << "num2 : ";
+  cin >> num2;
+
+  cout << "Result Addition    : " << add(num1,num2) << endl;
+  cout << "Result Subtraction : " << sub(num1,num2) << endl;
+
+}
